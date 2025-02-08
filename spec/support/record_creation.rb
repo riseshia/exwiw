8c091ad75bb29f22533d26efdbdb92ab87386c3a
Shia <rise.shia@gmail.com>
2025-02-08 11:24:47 +0900

    Add scaffold for Order

--- spec/support/record_creation.rb
+++ spec/support/record_creation.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 5.times do |i|
-  shop = Shop.create(name: "Shop \\#{i + 1}")
+  shop = Shop.create(name: "Shop #{i + 1}")
+  users = []
   2.times do |j|
-    User.create(name: "User \\#{j + 1}", email: "user\\#{j + 1}@example.com", shop: shop)
+    users << User.create(name: "User #{j + 1}", email: "user#{j + 1}@example.com", shop: shop)
   end
+  products = []
   3.times do |k|
-    Product.create(name: "Product \\#{k + 1}", price: (k + 1) * 10, shop: shop)
+    products << Product.create(name: "Product #{k + 1}", price: (k + 1) * 10, shop: shop)
+  end
+  users.each do |user|
+    products.each do |product|
+      order = Order.create(shop: shop, user: user)
+      OrderItem.create(order: order, product: product)
+    end
   end
 end
