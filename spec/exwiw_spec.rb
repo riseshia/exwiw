8e0aed67d0521030d1faddadd80258687efb0a92
Shia <rise.shia@gmail.com>
2025-02-08 10:47:37 +0900

    Extract database schema tests

--- spec/exwiw_spec.rb
+++ spec/exwiw_spec.rb
@@ -5,33 +5,3 @@ RSpec.describe Exwiw do
     expect(Exwiw::VERSION).not_to be nil
   end
 end
-
-RSpec.describe 'Database schema' do
-  it 'creates the companies table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:companies)).to be true
-  end
-
-  it 'creates the users table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:users)).to be true
-  end
-
-  it 'creates the products table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:products)).to be true
-  end
-
-  it 'creates the orders table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:orders)).to be true
-  end
-
-  it 'creates the order_items table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:order_items)).to be true
-  end
-
-  it 'creates the transactions table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:transactions)).to be true
-  end
-
-  it 'creates the reviews table' do
-    expect(ActiveRecord::Base.connection.table_exists?(:reviews)).to be true
-  end
-end
