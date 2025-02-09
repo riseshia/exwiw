627f96d920c68dc70a93d2aae51a0e63c16a922e
Shia <rise.shia@gmail.com>
2025-02-09 20:05:11 +0900

    misc

--- lib/exwiw/runner.rb
+++ lib/exwiw/runner.rb
@@ -22,8 +22,8 @@ module Exwiw
       end
 
       ordered_table_names.each do |table_name|
-        query = QueryAstBuilder.run(table_name, config.tables, @dump_target)
-        results = adapter.execute(query)
+        query_ast = QueryAstBuilder.run(table_name, config.tables, @dump_target)
+        result = adapter.execute(query_ast)
         insert_sql = adapter.to_bulk_insert(results, table_name)
 
         File.open(File.join(@output_dir, "#{table_name}.sql"), 'w') do |file|
